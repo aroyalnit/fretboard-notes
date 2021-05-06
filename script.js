d753b2a467453d9aef63ca01192c2bc070b079fb
-l <ian.taylor@gnsworldwide.com>
2021-05-06 15:03:50 +0100

    Move styles / script to external places

---
+++ script.js
@@ -0,0 +1,120 @@
+let notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+let strings = ['E', 'A', 'D', 'G', 'B']
+let variations = ['natural', 'flat', 'sharp']
+
+let variationSymbols = {
+  'natural': '',
+  'flat': '&flat;',
+  'sharp': '&sharp;'
+}
+
+let stringSelections = {}
+notes.forEach((note) => {
+  stringSelections[note] = true
+})
+
+let variationSelections = {}
+variations.forEach((variation) => {
+  variationSelections[variation] = true
+})
+
+let timer
+let allNotes = []
+let index = 0
+
+window.onload = function() {
+
+  document.getElementById("content").addEventListener("click", function(e){
+    doNextNote()
+  })
+
+  document.getElementById("settings-menu").addEventListener("click", function(e) {
+    let currentDisplay = window.getComputedStyle(document.getElementById("settings-expand"), null).getPropertyValue("display")
+    let newDisplay = currentDisplay == 'none' ? 'inline' : 'none'
+    document.getElementById("settings-expand").style.display = newDisplay
+  })
+
+  document.getElementById('currentNote').innerHTML = allNotes[index]
+  document.getElementById('nextNote').innerHTML = `NEXT (${index+1} of ${allNotes.length})`
+  setTimer()
+  try {
+    navigator.wakeLock.request('screen')
+  } catch (err) {
+    console.error(`${err.name}, ${err.message}`)
+  }
+
+  let stringForm = document.getElementById('strings')
+  stringForm.addEventListener('change', (event) => {
+    let elements = stringForm.elements
+    strings.forEach((string) => {
+      stringSelections[string] = elements[string].checked
+    })
+    setAllNotes()
+  })
+
+  let variationsForm = document.getElementById('variations')
+  variationsForm.addEventListener('change', (event) => {
+    let elements = variationsForm.elements
+    variations.forEach((variation) => {
+      variationSelections[variation] = elements[variation].checked
+    })
+    setAllNotes()
+  })
+  setAllNotes()
+}
+
+function setAllNotes()
+{
+  allNotes = []
+  strings.forEach((string) => {
+    if (stringSelections[string]) {
+      notes.forEach((note) => {
+        variations.forEach((variation) => {
+          if (variationSelections[variation]) {
+            allNotes.push(`<b>${note}${variationSymbols[variation]}</b> on <b>${string}</b> string`)
+          }
+        })
+      })
+    }
+  })
+  reset()
+}
+
+function reset() {
+  index = 0
+  allNotes = allNotes.sort( () => .5 - Math.random())
+  doNextNote()
+}
+
+function doNextNote() {
+  relax()
+  index++
+  if (index == allNotes.length) {
+    document.getElementById('currentNote').innerHTML = 'ALL COMPLETE!'
+    document.getElementById('nextNote').innerHTML = `Reset: ${allNotes.length} complete`
+  }
+  else if (index > allNotes.length) {
+    index = 0;
+    doNextNote()
+  }
+  else {
+    setTimer()
+    document.getElementById('currentNote').innerHTML = allNotes[index]
+    document.getElementById('nextNote').innerHTML = `NEXT (${index+1} of ${allNotes.length})`
+  }
+}
+
+function setTimer() {
+  timer = setTimeout(() => {
+    panic()
+  }, 4000)
+}
+
+function relax() {
+  clearTimeout(timer)
+  document.body.style.background = 'white'
+}
+
+function panic() {
+  document.body.style.background = 'red'
+}
